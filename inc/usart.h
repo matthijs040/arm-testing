7b4dd7166f262aa0ce191db88f86e96ab8cf9797
Matthijs Besselink <matthijsbesselink@live.nl>
2021-05-17 16:02:09 +0200

    stripped down the example to just usart. moved it to a dedicated header.

---
+++ inc/usart.h
@@ -0,0 +1,54 @@
+#pragma once
+
+// For intellisense.
+#ifndef STM32F3
+#define STM32F3
+#endif
+
+#include <stdio.h>
+#include <errno.h>
+
+#include <libopencm3/stm32/gpio.h>
+#include <libopencm3/stm32/rcc.h>
+#include <libopencm3/stm32/usart.h>
+
+int _write(int file, char *ptr, int len);
+
+static void usart_setup(void)
+{
+	/* Enable clocks for GPIO port A (for GPIO_USART2_TX) and USART2. */
+	rcc_periph_clock_enable(RCC_USART2);
+	rcc_periph_clock_enable(RCC_GPIOA);
+
+	/* Setup GPIO pin GPIO_USART2_TX/GPIO9 on GPIO port A for transmit. */
+	gpio_mode_setup(GPIOA, GPIO_MODE_AF, GPIO_PUPD_NONE, GPIO2 | GPIO3);
+	gpio_set_af(GPIOA, GPIO_AF7, GPIO2| GPIO3);
+
+	/* Setup UART parameters. */
+	usart_set_baudrate(USART2, 115200);
+	usart_set_databits(USART2, 8);
+	usart_set_stopbits(USART2, USART_STOPBITS_1);
+	usart_set_mode(USART2, USART_MODE_TX_RX);
+	usart_set_parity(USART2, USART_PARITY_NONE);
+	usart_set_flow_control(USART2, USART_FLOWCONTROL_NONE);
+
+	/* Finally enable the USART. */
+	usart_enable(USART2);
+}
+
+int _write(int file, char *ptr, int len)
+{
+        int i;
+
+        if (file == 1) {
+                for (i = 0; i < len; i++) {
+			if (ptr[i] == '\n') {
+				usart_send_blocking(USART2, '\r');
+			}
+                        usart_send_blocking(USART2, ptr[i]);
+		}
+                return i;
+        }
+        errno = EIO;
+        return -1;
+}
--- main.c
+++ main.c
@@ -3,15 +3,19 @@
 #define STM32F3
 #endif
 
+#include "inc/usart.h"
+
 #include <errno.h>
 #include <stdio.h>
+#include <stdlib.h>
+
 #include <libopencm3/stm32/rcc.h>
 #include <libopencm3/stm32/usart.h>
 #include <libopencm3/stm32/i2c.h>
 #include <libopencm3/stm32/gpio.h>
 
-int _write(int file, char *ptr, int len);
 
+/**
 static void i2c_setup(void)
 {
 	rcc_periph_clock_enable(RCC_I2C1);
@@ -19,14 +23,14 @@ static void i2c_setup(void)
 	rcc_set_i2c_clock_hsi(I2C1);
 
 	i2c_reset(I2C1);
-	/* Setup GPIO pin GPIO_USART2_TX/GPIO9 on GPIO port A for transmit. */
+	// Setup GPIO pin GPIO_USART2_TX/GPIO9 on GPIO port A for transmit. 
 	gpio_mode_setup(GPIOB, GPIO_MODE_AF, GPIO_PUPD_NONE, GPIO6 | GPIO7);
 	gpio_set_af(GPIOB, GPIO_AF4, GPIO6 | GPIO7);
 	i2c_peripheral_disable(I2C1);
 	//configure ANFOFF DNF[3:0] in CR1
 	i2c_enable_analog_filter(I2C1);
 	i2c_set_digital_filter(I2C1, 0);
-	/* HSI is at 8Mhz */
+	// HSI is at 8Mhz 
 	i2c_set_speed(I2C1, i2c_speed_sm_100k, 8);
 	//configure No-Stretch CR1 (only relevant in slave mode)
 	i2c_enable_stretching(I2C1);
@@ -34,29 +38,10 @@ static void i2c_setup(void)
 	i2c_set_7bit_addr_mode(I2C1);
 	i2c_peripheral_enable(I2C1);
 }
+**/
 
-static void usart_setup(void)
-{
-	/* Enable clocks for GPIO port A (for GPIO_USART2_TX) and USART2. */
-	rcc_periph_clock_enable(RCC_USART2);
-	rcc_periph_clock_enable(RCC_GPIOA);
 
-	/* Setup GPIO pin GPIO_USART2_TX/GPIO9 on GPIO port A for transmit. */
-	gpio_mode_setup(GPIOA, GPIO_MODE_AF, GPIO_PUPD_NONE, GPIO2 | GPIO3);
-	gpio_set_af(GPIOA, GPIO_AF7, GPIO2| GPIO3);
-
-	/* Setup UART parameters. */
-	usart_set_baudrate(USART2, 115200);
-	usart_set_databits(USART2, 8);
-	usart_set_stopbits(USART2, USART_STOPBITS_1);
-	usart_set_mode(USART2, USART_MODE_TX_RX);
-	usart_set_parity(USART2, USART_PARITY_NONE);
-	usart_set_flow_control(USART2, USART_FLOWCONTROL_NONE);
-
-	/* Finally enable the USART. */
-	usart_enable(USART2);
-}
-
+/**
 static void gpio_setup(void)
 {
 	rcc_periph_clock_enable(RCC_GPIOE);
@@ -64,30 +49,14 @@ static void gpio_setup(void)
 		GPIO8 | GPIO9 | GPIO10 | GPIO11 | GPIO12 | GPIO13 |
 		GPIO14 | GPIO15);
 }
+**/
 
-int _write(int file, char *ptr, int len)
-{
-        int i;
-
-        if (file == 1) {
-                for (i = 0; i < len; i++) {
-			if (ptr[i] == '\n') {
-				usart_send_blocking(USART2, '\r');
-			}
-                        usart_send_blocking(USART2, ptr[i]);
-		}
-                return i;
-        }
-        errno = EIO;
-        return -1;
-}
-
-
+/**
 static void clock_setup(void)
 {
 	rcc_clock_setup_hsi(&rcc_hsi_configs[RCC_CLOCK_HSI_64MHZ]);
 }
-
+**/
 #define I2C_ACC_ADDR 0x19
 #define I2C_MAG_ADDR 0x1E
 #define ACC_STATUS 0x27
@@ -102,31 +71,31 @@ static void clock_setup(void)
 
 int main(void)
 {
-	clock_setup();
-	gpio_setup();
+	// clock_setup();
+	// gpio_setup();
 	usart_setup();
-	printf("Hello, we're running\n");
-	i2c_setup();
-	uint8_t cmd = ACC_CTRL_REG1_A;
-	uint8_t data;
-	i2c_transfer7(I2C1, I2C_ACC_ADDR, &cmd, 1, &data, 1);
-	cmd = ACC_CTRL_REG4_A;
-	i2c_transfer7(I2C1, I2C_ACC_ADDR, &cmd, 1, &data, 1);
-	int16_t acc_x;
+	puts("Hello, we're running");
+	// i2c_setup();
+	// uint8_t cmd = ACC_CTRL_REG1_A;
+	// uint8_t data;
+	// i2c_transfer7(I2C1, I2C_ACC_ADDR, &cmd, 1, &data, 1);
+	// cmd = ACC_CTRL_REG4_A;
+	// i2c_transfer7(I2C1, I2C_ACC_ADDR, &cmd, 1, &data, 1);
+	// int16_t acc_x;
+// 
+	// while (1) {
+// 
+	// 	cmd = ACC_STATUS;
+	// 	i2c_transfer7(I2C1, I2C_ACC_ADDR, &cmd, 1, &data, 1);
+	// 	cmd = ACC_OUT_X_L_A;
+	// 	i2c_transfer7(I2C1, I2C_ACC_ADDR, &cmd, 1, &data, 1);
+	// 	acc_x = data;
+	// 	cmd = ACC_OUT_X_H_A;
+	// 	i2c_transfer7(I2C1, I2C_ACC_ADDR, &cmd, 1, &data, 1);
+	// 	acc_x |= ((uint16_t)data << 8);
+	// 	printf("data was %d\n", acc_x);
+	// }
 
-	while (1) {
-
-		cmd = ACC_STATUS;
-		i2c_transfer7(I2C1, I2C_ACC_ADDR, &cmd, 1, &data, 1);
-		cmd = ACC_OUT_X_L_A;
-		i2c_transfer7(I2C1, I2C_ACC_ADDR, &cmd, 1, &data, 1);
-		acc_x = data;
-		cmd = ACC_OUT_X_H_A;
-		i2c_transfer7(I2C1, I2C_ACC_ADDR, &cmd, 1, &data, 1);
-		acc_x |= ((uint16_t)data << 8);
-		printf("data was %d\n", acc_x);
-	}
-
-	return 0;
+	return EXIT_SUCCESS;
 }
 
