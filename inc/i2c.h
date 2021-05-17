35f5d028809527b0885a168c619893c552104184
Matthijs Besselink <matthijsbesselink@live.nl>
2021-05-17 17:57:20 +0200

    recommit of first attempt at i2c.

--- inc/i2c.h
+++ inc/i2c.h
@@ -17,24 +17,25 @@ static void i2c_setup(void)
 	rcc_periph_clock_enable(RCC_GPIOB);
 	rcc_set_i2c_clock_hsi(I2C1);
 
-	i2c_reset(I2C1);
+    i2c_reset(I2C1);
 	// Setup GPIO pin 6 and 7 on GPIO port B for alternate function. 
-	gpio_mode_setup(GPIOB, GPIO_MODE_AF, GPIO_PUPD_NONE, GPIO6 | GPIO7);
+	gpio_mode_setup(GPIOB, GPIO_MODE_AF, GPIO_PUPD_NONE, GPIO8 | GPIO9);
     // Set alternate function to i2c.
     // In page 55 of datasheet. I2C1-SCL and SDA.
-	gpio_set_af(GPIOB, GPIO_AF4, GPIO6 | GPIO7);
+	gpio_set_af(GPIOB, GPIO_AF4, GPIO8 | GPIO9);
 	i2c_peripheral_disable(I2C1);
 	//configure ANFOFF DNF[3:0] in CR1
 	i2c_enable_analog_filter(I2C1);
 	i2c_set_digital_filter(I2C1, 0);
 	// HSI is at 8Mhz 
-	i2c_set_speed(I2C1, i2c_speed_fm_400k, 8);
+	i2c_set_speed(I2C1, i2c_speed_fm_400k, 64);
 	//configure No-Stretch CR1 (only relevant in slave mode)
 	i2c_enable_stretching(I2C1);
 	//addressing mode
 	i2c_set_7bit_addr_mode(I2C1);
 
 	i2c_peripheral_enable(I2C1);
+    
 }
 
 #define MPU_I2C_ADDR_LOW 104 // 0b1101000   if AD0 pin on the mpu is low
